560833e0be66f7810f38fd733e0ba6dbc16a121d
DolphinGamerYT <dolphingamerytof@gmail.com>
2022-04-03 12:38:36 +0200

    Command to kill unused traps

--- src/main/java/me/aleiv/gameengine/games/beast/trap/TrapCommand.java
+++ src/main/java/me/aleiv/gameengine/games/beast/trap/TrapCommand.java
@@ -3,21 +3,24 @@ package me.aleiv.gameengine.games.beast.trap;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
-import co.aikar.commands.annotation.PreCommand;
 import me.aleiv.gameengine.Core;
 import me.aleiv.gameengine.games.beast.BeastEngine;
 import me.aleiv.gameengine.games.beast.config.BeastMapConfig;
 import me.aleiv.gameengine.globalUtilities.EngineEnums;
 import me.aleiv.gameengine.gui.ConfigMenu;
-import me.aleiv.gameengine.gui.LocationsGUI;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @CommandAlias("trap")
 @CommandPermission("game.beast.trap")
@@ -31,6 +34,28 @@ public class TrapCommand extends BaseCommand {
 
     private BeastMapConfig getConfig(String mapName) {
         return this.engine.getBeastConfig().getMap(mapName);
+    }
+
+    @CommandAlias("kill")
+    public void onkill(Player player) {
+        List<Integer> modeldatas = new ArrayList<>();
+        for (TrapAnimation value : TrapAnimation.values()) {
+            modeldatas.add(value.getInitCustomModelData());
+            modeldatas.add(value.getAnimateCustomModelData());
+        }
+
+        AtomicInteger traps = new AtomicInteger();
+        player.getWorld().getEntities().stream().filter(e -> e.getType() == EntityType.ARMOR_STAND).map(e -> (ArmorStand) e).forEach(e -> {
+            if (e.getEquipment() == null || e.getEquipment().getHelmet() == null || e.getEquipment().getHelmet().getType() != Material.BRICK) return;
+            ItemStack b = e.getEquipment().getHelmet();
+            if (!b.hasItemMeta()) return;
+            if (modeldatas.contains(b.getItemMeta().getCustomModelData())) {
+                e.remove();
+                traps.getAndIncrement();
+            }
+        });
+
+        player.sendMessage("Killed " + traps + " traps");
     }
 
     @CommandAlias("adamage")
